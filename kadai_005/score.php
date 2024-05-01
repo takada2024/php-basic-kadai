6d99af147cd1dbb943a785d204ea1d59f2e96428
takada2024 <165872660+takada2024@users.noreply.github.com>
2024-05-01 10:20:16 +0900

    提出用：kadai_005をアップロード

---
+++ kadai_005/score.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>平均62.5</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 変数 代入 一覧
+        $score1 = '80';
+        $score2 = '60';
+        $score3 = '55';
+        $score4 = '40';
+        $score5 = '100';
+        $score6 = '25';
+        $score7 = '80';
+        $score8 = '95';
+        $score9 = '30';
+        $score10 = '60';
+
+        // 合計
+        $score_sum = ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10);
+
+        // 平均
+        $score_average = $score_sum / 10;
+
+        echo '合計：' . $score_sum;
+        echo '<br>';
+        echo '平均：' . $score_average
+        ?>
+    </p>
+</body>
+
+</html>
