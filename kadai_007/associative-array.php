919673c09296f430862642edee86ee3bd631177e
takada2024 <165872660+takada2024@users.noreply.github.com>
2024-05-01 17:06:56 +0900

    提出用：kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>連想配列</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $Array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($Array)
+        ?>
+    </p>
+</body>
+
+</html>
